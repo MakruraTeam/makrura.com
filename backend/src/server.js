f5ec7ad52505dde5d1d4c4c8db55115b51331a0d
DwieTwarze2000 <pawelbinkowski2000@gmail.com>
2025-11-03 18:17:43 +0100

    feat: add server build

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,9 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
+import path from 'path';
+import process from 'process';
+
 import { connectToDatabase } from './db.js';
 import { setupDatabase } from './setupDatabase.js';
 import authRoutes from './auth/routes/auth.routes.js';
@@ -9,15 +12,34 @@ import authRoutes from './auth/routes/auth.routes.js';
 dotenv.config();
 
 const app = express();
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/api/auth', authRoutes);
 
-await connectToDatabase();
-await setupDatabase();
+// build
+const publicPath = path.join(process.cwd(), 'public');
+app.use(express.static(publicPath));
+app.get(/.*\.(js|css)$/, (req, res, next) => {
+  res.set('Cache-Control', 'public, max-age=31536000');
+  next();
+});
+app.get(/.*/, (req, res, next) => {
+  if (req.path.startsWith('/api/')) {
+    return next();
+  }
 
-app.listen(process.env.PORT || 3000, async () => {
-  console.log(`Server running on port ${process.env.PORT || 3000}`);
+  res.sendFile(path.join(publicPath, 'index.html'), (err) => {
+    if (err) next(err);
+  });
+});
+
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, async () => {
+  await connectToDatabase();
+  await setupDatabase();
+  console.log(`Server running on http://localhost:${PORT}`);
 });
